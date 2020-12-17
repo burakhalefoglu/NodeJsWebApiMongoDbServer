bce20e6d93f0afb3c66ec9a09f249a94b3138697
burakhalefoglu <42536490+burakhalefoglu@users.noreply.github.com>
2020-12-17 23:11:36 +0300

    Last
    Last

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+    PORT: 3000,
+    DB_URL: "mongodb+srv://DbUser:Developer123@cluster0.dggm8.mongodb.net/appneuron?retryWrites=true&w=majority",
+    SECRET_KEY: "burak"
+
+  };
+  
+  module.exports = config;
